e39b11c3d60f68837f7ab80ee45e02a944068adb
Marc Lehmann <schmorp@schmorp.de>
2009-08-05 11:51:48 +0000

    Import of MLEHMANN/Async-Interrupt-1.01 from CPAN.
    gitpan-cpan-distribution: Async-Interrupt
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         MLEHMANN/Async-Interrupt-1.01.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

--- Interrupt.pm
+++ Interrupt.pm
@@ -238,7 +238,7 @@ BEGIN {
    # etc. will be null pointers.
    $SIG{KILL} = sub { };
 
-   our $VERSION = '1.0';
+   our $VERSION = '1.01';
 
    require XSLoader;
    XSLoader::load ("Async::Interrupt", $VERSION);
